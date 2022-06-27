1304dba8ac44d8e832985173aa59d314525391cc
rahul21200 <rahulkapoor902@gmail.com>
2022-06-27 17:35:34 +0530

    index.html changed

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from io import StringIO
 # print("hello")
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def showCSV(uploaded_file):
@@ -10,6 +12,8 @@ def showCSV(uploaded_file):
         # Can be used wherever a "file-like" object is accepted:
         dataframe = pd.read_csv(uploaded_file)
         st.write(dataframe)
+        st.line_chart(data=dataframe, width=0, height=0,
+                      use_container_width=True)
 
 
 uploaded_file = st.file_uploader("Choose a file")
