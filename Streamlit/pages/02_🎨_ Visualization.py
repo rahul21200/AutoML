3dd63f7cfe97598c672c6ac21fdbffd90280fdd7
rahul21200 <rahulkapoor902@gmail.com>
2022-09-06 15:34:48 +0530

    path changes

--- Streamlit/pages/02_🎨_ Visualization.py
+++ Streamlit/pages/02_🎨_ Visualization.py
@@ -13,7 +13,6 @@ def generateGraphs():
 
 
 generateGraphs()
-
 for graphs in os.listdir('../AutoViz_plots/AutoViz'):
     if graphs.endswith(".png"):
         st.image('../AutoViz_plots/AutoViz/'+graphs, use_column_width=True)
